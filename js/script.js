f0a07b9f5ffea3ff1156f771aae6df76285aa94b
diamant1991 <diamant1988@mail.ru>
2015-06-04 17:35:49 +0300

    first files

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('input,textarea').focus(function(){
+  $(this).data('placeholder',$(this).attr('placeholder'))
+  $(this).attr('placeholder','');
+});
+$('input,textarea').blur(function(){
+  $(this).attr('placeholder',$(this).data('placeholder'));
+});
+
+$( document ).ready(function() {
+
+	$('.fav-link').click(function(){
+		$(this).parent('.favorites').toggleClass('favorites_open');
+	})
+
+	$('.arend-link').click(function(){
+    	$('.form-mask').fadeIn(400);
+    	$('.modal').fadeIn(400);
+    })
+
+    $('.form-mask,.closed').click(function(){
+  	 $('.modal,.form-mask').fadeOut(400);
+  })
+	$(document).keyup(function(d) {
+	    if (d.keyCode == 27) {
+	        $('.form-mask').fadeOut(400);
+	        $('.modal').fadeOut(400);
+	    }
+	});
+
+
+});
